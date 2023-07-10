06955ccb48f65e439cdb4a23d6e817c671b10e16
Josh Wittlieff <josh@techfrog.io>
2023-07-10 16:27:57 -0500

    Add auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator, Button } from "@aws-amplify/ui-react";
 
-function App() {
+function App({ signOut }) {
   return (
     <div className="App">
       <header className="App-header">
@@ -9,8 +10,8 @@ function App() {
         <p>Lots of hopping up, across, and down on bikes.</p>
         <p>And occassionally crashing.</p>
       </header>
+      <Button onClick={signOut}>Sign Out</Button>
     </div>
   );
 }
-
-export default App;
+export default withAuthenticator(App);
