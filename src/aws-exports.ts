31498667adbe47223f64bb759d6c53388db4a876
Josh Wittlieff <josh@techfrog.io>
2023-07-21 00:33:00 -0500

    Package setup

---
+++ src/aws-exports.ts
@@ -0,0 +1,26 @@
+﻿const awsmobile = {
+  aws_project_region: "us-east-2",
+  aws_cognito_identity_pool_id:
+    "us-east-2:347d51bd-333b-4a96-814b-c29d7acd97df",
+  aws_cognito_region: "us-east-2",
+  aws_user_pools_id: "us-east-2_Dy3cmjmsE",
+  aws_user_pools_web_client_id: "57rsmkea8qg3t38m0fg4l6cufn",
+  oauth: {},
+  aws_cognito_username_attributes: [],
+  aws_cognito_social_providers: [],
+  aws_cognito_signup_attributes: ["EMAIL"],
+  aws_cognito_mfa_configuration: "OFF",
+  aws_cognito_mfa_types: ["SMS"],
+  aws_cognito_password_protection_settings: {
+    passwordPolicyMinLength: 8,
+    passwordPolicyCharacters: [],
+  },
+  aws_cognito_verification_mechanisms: ["EMAIL"],
+  aws_appsync_graphqlEndpoint:
+    "https://fd67lj2ygbdyboehanbpdrpjl4.appsync-api.us-east-2.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-2",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-iylrdnjmhvdjxhqvnzuo44rr7q",
+};
+
+export default awsmobile;
